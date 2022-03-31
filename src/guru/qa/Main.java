c2a0031e1c5404cc0feb0bb5d22581f694bee620
Andrei Gordey <91781472+AGordey@users.noreply.github.com>
2022-03-31 15:44:42 +0300

    Update Main.java

--- src/guru/qa/Main.java
+++ src/guru/qa/Main.java
@@ -4,9 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
     System.out.println ("Hello QA GURU");
-    System.out.println ("Googbue ");
-    System.out.println ("Change for second commit");
-    System.out.println ("Line for new_branch");
     System.out.println ("Line for new_branch");
     }
 }
